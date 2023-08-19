0d5a4b9189629cc09a69be5e5f8dec8d04d14702
Max <Max@DESKTOP-KU6J5I4>
2023-08-19 17:39:30 +0300

    Добавьте файлы проекта.

---
+++ pohuy/Config.cs
@@ -0,0 +1,17 @@
+﻿using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pohuy
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+
+        public ushort Time { get; set; } = 5;
+    }
+}
---
+++ pohuy/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.Extensions;
+using Exiled.Events.Handlers;
+using MEC;
+using System.Collections.Generic;
+
+namespace pohuy
+{
+    public class Plugin: Plugin<Config>
+    {
+        public override string Author => "yjjz";
+        public override string Name => "pohuy";
+        public override string Prefix => "ph";
+        public static Plugin Instance { get; } = new();
+
+        public static Config Configs = Instance.Config;
+        private Plugin()
+        {
+        }
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.Verified += Player_Verified;
+        }
+
+        private void Player_Verified(Exiled.Events.EventArgs.Player.VerifiedEventArgs ev)
+        {
+            Timing.RunCoroutine(MyCoroutine(ev.Player));
+        }
+
+        public IEnumerator<float> MyCoroutine(Exiled.API.Features.Player player)
+        {
+            for (; ; )
+            {
+                player.Broadcast(Configs.Time,"kek");
+                yield return Timing.WaitForSeconds(5f);
+            }
+        }
+    }
+}
